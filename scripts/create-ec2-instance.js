06896654f78e1300e3a6b65dbadb2b956e74d4dc
chill <styr13@hotmail.com>
2023-04-16 16:06:24 +0800

    setup updates

--- scripts/create-ec2-instance.js
+++ scripts/create-ec2-instance.js
@@ -22,9 +22,9 @@ const keyName = 'hamster_key'
 // Do all the things together
 async function execute () {
   try {
-    await createSecurityGroup(sgName)
-    const keyPair = await createKeyPair(keyName)
-    await helpers.persistKeyPair(keyPair)
+    //await createSecurityGroup(sgName)
+    //const keyPair = await createKeyPair(keyName)
+    //await helpers.persistKeyPair(keyPair)
     const data = await createInstance(sgName, keyName)
     console.log('Created instance with:', data)
   } catch (err) {
@@ -71,7 +71,7 @@ async function createKeyPair (keyName) {
 }
 
 async function createInstance (sgName, keyName) {
-   const params = {
+  const params = {
     // From AWS Amazon Linux 2023 AMI(Amazon Machine Image) ID
     ImageId: 'ami-0d9f286195031c3d9',
     InstanceType: 't2.micro',
@@ -79,9 +79,8 @@ async function createInstance (sgName, keyName) {
     SecurityGroups:[sgName],
     MaxCount: 1,
     MinCount:1,
-    UserData: "IyEvYmluL2Jhc2gKY3VybCAtLXNpbGVudCAtLWxvY2F0aW9uIGh0dHBzOi8vcnBtLm5vZGVzb3VyY2UuY29tL3NldHVwXzE2LnggfCBzdWRvIGJhc2ggLQpzdWRvIHl1bSBpbnN0YWxsIC15IG5vZGVqcwpzdWRvIHl1bSBpbnN0YWxsIC15IGdpdApjZCBob21lL2VjMi11c2VyCmdpdCBjbG9uZSBodHRwczovL2dpdGh1Yi5jb20vQ2FtSGlsbDcxL2F3cy1kZGQuZ2l0CmNkIGF3cy1kZGQKbnBtIGkKbnBtIHJ1biBzdGFydA=="
-  }
-
+    UserData: "IyEvYmluL2Jhc2gKY3VybCAtby0gaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL252bS1zaC9udm0vdjAuMzkuMy9pbnN0YWxsLnNoIHwgYmFzaApzdWRvIHl1bSBpbnN0YWxsIC15IG5vZGVqcwpzdWRvIHl1bSBpbnN0YWxsIC15IGdpdApjZCAvaG9tZS9lYzItdXNlcgpnaXQgY2xvbmUgaHR0cHM6Ly9naXRodWIuY29tL0NhbUhpbGw3MS9hd3MtZGRkLmdpdApjZCBhd3MtZGRkCnN1ZG8gbnBtIHJ1biBidWlsZApzdWRvIG5wbSBpCm5wbSBydW4gc3RhcnQK"
+  };
   const command = new RunInstancesCommand(params);
   return sendCommand(command);
 }
--- scripts/ec2-startup.sh
+++ scripts/ec2-startup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 sudo yum install -y nodejs
 sudo yum install -y git
-cd home/ec2-user
+cd /home/ec2-user
 git clone https://github.com/CamHill71/aws-ddd.git
 cd aws-ddd
-npm i
+sudo npm run build
+sudo npm i
 npm run start
 
 # The above commands base64 encoded for entering into UserData
-# IyEvYmluL2Jhc2gKY3VybCAtLXNpbGVudCAtLWxvY2F0aW9uIGh0dHBzOi8vcnBtLm5vZGVzb3VyY2UuY29tL3NldHVwXzE2LnggfCBzdWRvIGJhc2ggLQpzdWRvIHl1bSBpbnN0YWxsIC15IG5vZGVqcwpzdWRvIHl1bSBpbnN0YWxsIC15IGdpdApjZCBob21lL2VjMi11c2VyCmdpdCBjbG9uZSBodHRwczovL2dpdGh1Yi5jb20vQ2FtSGlsbDcxL2F3cy1kZGQuZ2l0CmNkIGF3cy1kZGQKbnBtIGkKbnBtIHJ1biBzdGFydA==
+# IyEvYmluL2Jhc2gKY3VybCAtby0gaHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL252bS1zaC9udm0vdjAuMzkuMy9pbnN0YWxsLnNoIHwgYmFzaApzdWRvIHl1bSBpbnN0YWxsIC15IG5vZGVqcwpzdWRvIHl1bSBpbnN0YWxsIC15IGdpdApjZCAvaG9tZS9lYzItdXNlcgpnaXQgY2xvbmUgaHR0cHM6Ly9naXRodWIuY29tL0NhbUhpbGw3MS9hd3MtZGRkLmdpdApjZCBhd3MtZGRkCnN1ZG8gbnBtIHJ1biBidWlsZApzdWRvIG5wbSBpCm5wbSBydW4gc3RhcnQK
